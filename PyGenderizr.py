6de8bc1df51a89d4669c9900c2e804e04814afcc
Fabián Villena <villenafabian@gmail.com>
2018-02-18 00:20:08 -0300

    Initial commit

---
+++ PyGenderizr.py
@@ -0,0 +1,25 @@
+import csv
+
+def PyGenderizr(name):
+    name = name.lower()
+
+    with open('maleNames.csv', 'rt') as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            for field in row:
+                if field == name:
+                    return "male"
+
+    with open('femaleNames.csv', 'rt') as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            for field in row:
+                if field == name:
+                    return "female"
+
+    return "i"
+
+if __name__ == "__main__":
+    import sys
+
+    print(PyGenderizr(str(sys.argv[1])))
---
+++ demo.py
@@ -0,0 +1,3 @@
+from PyGenderizr import *
+
+print(PyGenderizr("Juan"))
